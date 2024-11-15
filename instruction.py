aabca91212404b249692f0640fe36f828011d728
katvus <kat.vus8@gmail.com>
2024-11-15 16:12:29 +0300

    add class Operation

--- instruction.py
+++ instruction.py
@@ -3,78 +3,95 @@ import enum
 from exception import MyProgramError
 
 
-def load_reg(rd, const, proc):
-    proc.registers[rd] = const
-    proc.pc += 1
+class Operation:
+    @staticmethod
+    def load_reg(rd, const, proc):
+        proc.registers[rd] = const
+        proc.pc += 1
 
-
-def jump(rd, const, proc):
-    proc.registers[rd] = proc.pc + 1
-    proc.pc += const
-
-
-def cond_jump(rd, const, proc):
-    if (proc.registers[rd]) >= 0:
+    @staticmethod
+    def jump(rd, const, proc):
+        proc.registers[rd] = proc.pc + 1
         proc.pc += const
-    else:
+
+    @staticmethod
+    def cond_jump(rd, const, proc):
+        if (proc.registers[rd]) >= 0:
+            proc.pc += const
+        else:
+            proc.pc += 1
+
+    @staticmethod
+    def store_mem(rd, rbase, const, proc):
+        proc.registers[rd] = proc.mem[proc.registers[rbase] + const]
         proc.pc += 1
 
-
-def store_mem(rd, rbase, const, proc):
-    proc.registers[rd] = proc.mem[proc.registers[rbase] + const]
-    proc.pc += 1
-
-
-def load_mem(rs, rbase, const, proc):
-    proc.mem[proc.registers[rbase] + const] = proc.registers[rs]
-    proc.pc += 1
-
-
-def reg_jump(rd, rbase, const, proc):
-    proc.registers[rd] = proc.pc + 1
-    proc.pc = proc.registers[rbase] + const
-
-
-dict_t_op = {'изп': store_mem, 'вп': load_mem, 'опер': reg_jump}
-
-
-dict_s_op = {'вр': load_reg, 'пер': jump, 'усл': cond_jump}
-
-
-dict_f_op = {'сл': lambda a, b: a + b, 'выч': lambda a, b: a - b,
-             'умн': lambda a, b: a * b, 'дел': lambda a, b: a / b,
-             'и': lambda a, b: a & b, 'или': lambda a, b: a | b,
-             'свл': lambda a, b: a << b, 'свп': lambda a, b: a >> b,
-             'иили': lambda a, b: a ^ b}
-
-
-def f_op_proc(data, proc):
-    if data[0] in dict_f_op:
-        proc.registers[register_number(data[1])] = dict_f_op[data[0]](
-            proc.registers[register_number(data[2])], proc.registers[register_number(data[3])])
+    @staticmethod
+    def load_mem(rs, rbase, const, proc):
+        proc.mem[proc.registers[rbase] + const] = proc.registers[rs]
         proc.pc += 1
-        return True
-    return False
+
+    @staticmethod
+    def reg_jump(rd, rbase, const, proc):
+        proc.registers[rd] = proc.pc + 1
+        proc.pc = proc.registers[rbase] + const
 
 
-def s_op_proc(data, proc):
-    if data[0] in dict_s_op:
-        if isint(data[2]) and -128 <= int(data[2]) <= 127:
-            dict_s_op[data[0]](register_number(data[1]), int(data[2]), proc)
+class Execution:
+    __dict_f_op = {'сл': lambda a, b: a + b, 'выч': lambda a, b: a - b,
+                   'умн': lambda a, b: a * b, 'дел': lambda a, b: a / b,
+                   'и': lambda a, b: a & b, 'или': lambda a, b: a | b,
+                   'свл': lambda a, b: a << b, 'свп': lambda a, b: a >> b,
+                   'иили': lambda a, b: a ^ b}
+    __dict_s_op = {'вр': Operation.load_reg, 'пер': Operation.jump, 'усл': Operation.cond_jump}
+    __dict_t_op = {'изп': Operation.store_mem, 'вп': Operation.load_mem, 'опер': Operation.reg_jump}
+
+    def __init__(self, processor, data):
+        self.proc = processor
+        self.data = data
+
+    def f_op(self):
+        if self.data[0] in self.__dict_f_op:
+            self.proc.registers[register_number(self.data[1])] = self.__dict_f_op[self.data[0]](
+                self.proc.registers[register_number(self.data[2])], self.proc.registers[register_number(self.data[3])])
+            self.proc.pc += 1
+            return True
+        return False
+
+    def s_op(self):
+        if self.data[0] in self.__dict_s_op:
+            if isint(self.data[2]) and -128 <= int(self.data[2]) <= 127:
+                self.__dict_s_op[self.data[0]](register_number(self.data[1]), int(self.data[2]), self.proc)
+            else:
+                raise MyProgramError('не является числом в возможном диапазоне')
         else:
-            raise MyProgramError('не является числом в возможном диапазоне')
-    else:
-        raise MyProgramError('недопустимая операция')
+            raise MyProgramError('недопустимая операция')
 
-
-def t_op_proc(data, proc):
-    if data[0] in dict_t_op:
-        if isint(data[3]) and -8 <= int(data[3]) <= 7:
-            dict_t_op[data[0]](register_number(data[1]), register_number(data[2]),  int(data[3]), proc)
+    def t_op(self):
+        if self.data[0] in self.__dict_t_op:
+            if isint(self.data[3]) and -8 <= int(self.data[3]) <= 7:
+                self.__dict_t_op[self.data[0]](register_number(self.data[1]), register_number(self.data[2]),  int(self.data[3]), self.proc)
+            else:
+                raise MyProgramError('не является числом в возможном диапазоне')
         else:
-            raise MyProgramError('не является числом в возможном диапазоне')
-    else:
-        raise MyProgramError('недопустимая операция')
+            raise MyProgramError('недопустимая операция')
+
+    def sys(self):
+        self.proc.pc += 1
+        if self.proc.registers[4] == SysCall.print.value:
+            print(self.proc.registers[5])
+        elif self.proc.registers[4] == SysCall.input.value:
+            self.proc.registers[5] = int(input())
+        elif self.proc.registers[4] == SysCall.exit.value:
+            exit()
+        else:
+            MyProgramError('недопустимый системный вызов')
+
+
+class SysCall(enum.Enum):
+    exit = 0
+    print = 1
+    input = 2
 
 
 def register_number(data):
@@ -88,21 +105,3 @@ def isint(data):
     if (data[0] == '-' and data[1:].isdigit()) or data.isdigit():
         return True
     return False
-
-
-class SysCall(enum.Enum):
-    exit = 0
-    print = 1
-    input = 2
-
-
-def sys(proc):
-    proc.pc += 1
-    if proc.registers[4] == SysCall.print.value:
-        print(proc.registers[5])
-    elif proc.registers[4] == SysCall.input.value:
-        proc.registers[5] = int(input())
-    elif proc.registers[4] == SysCall.exit.value:
-        exit()
-    else:
-        MyProgramError('недопустимый системный вызов')
